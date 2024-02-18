b52e11b2c47c77383684decf77face19d3e24502
Ecliße <rafaumar22@gmail.com>
2024-02-18 17:49:28 +0700

    vision & mission 2

--- database/migrations/2024_02_17_143951_create_missions_table.php
+++ database/migrations/2024_02_17_143951_create_missions_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('missions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('vote_num')->nullable();
-            $table->rememberToken();
+            $table->unsignedBigInteger('candidate_id');
+            $table->smallInteger('msn_sq_no');
+            $table->string('desc', 255);
             $table->timestamps();
+            $table->foreign('candidate_id')->references('id')->on('candidates')->onDelete('cascade');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('missions');
     }
 };
